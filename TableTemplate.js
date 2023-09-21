fc978dd866c08f37f758d997fb10dedd0a96640f
ItalianJack <92689827+ItalianJack@users.noreply.github.com>
2023-09-21 19:00:04 -0400

    initial commit for TableTemplate.js

---
+++ TableTemplate.js
@@ -0,0 +1,11 @@
+class TableTemplate {
+    static fillIn(tableID, dict, columnName) {
+        let table = document.getElementById(tableID);
+
+        // Fill in the header row
+        let row = table.rows[0];
+        for (let i = 0, cell; cell = row.cols[i]; i++) {
+            if (cell.content == )
+        }
+    }
+}
